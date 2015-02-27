c57d4dc1691d27ae0d7e12575088b94a36d799f2
Noel Alejandro <noel_ska15@hotmail.com>
2015-02-27 01:06:05 -0600

    Agregados handlers para la recepcion de datos y para cuando la conexion se cierra.

--- server.js
+++ server.js
@@ -6,10 +6,20 @@ var PORT = 666;
 var server = net.createServer(function(socket) {
 	console.log('CONNECTED: ' + socket.remoteAddress +':'+ socket.remotePort);
 	console.log("Server creado");
-	socket.write('Soy el server\r\n'); //Escribiendo contenido para que pueda leer el cliente
-	socket.pipe(socket); //Enviando el contenido por el mismo socket hacia el cliente
+	socket.buffer = new Buffer(2048);
+	socket.setEncoding('utf8');
+
+	socket.on('data', function(data) {
+		console.log('Received: ' + data);
+	});
+
+	socket.on('close', function() {
+		console.log('Connection closed');
+	}); 
+	//socket.write('Soy el server\r\n'); //Escribiendo contenido para que pueda leer el cliente
+	//socket.pipe(socket); //Enviando el contenido por el mismo socket hacia el cliente
 });
- 
+
 server.listen(PORT, HOST); 
 
 console.log('Server listening on ' + HOST +':'+ PORT);
